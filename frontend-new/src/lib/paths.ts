d1fd4d977997402dc6e9a9b516ec1dfb02a8b0d5
Caleb196x <liumingyuan_nuc@163.com>
2026-06-15 22:08:09 +0800

    feat: remove old frontend

--- frontend-new/src/lib/paths.ts
+++
@@ -1,6 +0,0 @@
-export const paths = {
-  projectList: () => '/projects',
-  projectDetail: (projectId: string) => `/projects/${projectId}`,
-  projectMembers: (projectId: string) => `/projects/${projectId}/members`,
-  projectSessions: (projectId: string) => `/projects/${projectId}/sessions`,
-};
